47647c8e53994d6f2e803f32e5620d9363947653
nmtu91174 <nmtu91174@gmail.com>
2025-08-06 15:05:46 +0700

    baiTapS3

---
+++ FPC/Week 02/HomeWorkS3/soTocanRut.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+	
+	int soTien;
+	int menhGia;
+	int soTo;
+	
+	printf("Nhap so tien can rut (boi so cua 10): ");
+    scanf("%d", &soTien);
+
+    if (soTien % 10 != 0 || soTien <= 0) {
+        printf("So tien phai la boi so cua 10 va lon hon 0.\n");
+        printf("\n");
+        
+		printf("Hay nhap lai!");
+        printf("\n");
+	}
+    
+    
+    
+    
+}
