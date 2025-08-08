39f2d30f361dc5e882300803fc0b78d65c2ac0e7
nmtu91174 <nmtu91174@gmail.com>
2025-08-08 10:07:26 +0700

    update code

---
+++ FPC/Week 02/Ss4/HomeWorkS4/giamGia.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(){
+	
+	float bill;
+	float pay;
+	float discount;
+	
+	printf("Nhap so tien trong hoa don: ");
+	scanf("%f",&bill);
+	
+	if (bill >= 500){
+		discount = bill * 0.1;
+		
+	} else if (bill >= 200){
+		discount = bill * 0.05;
+		
+		} else {
+			discount = 0;
+		}	 
+	
+	pay = bill - discount;
+	
+	printf("\n------------------------------\n");
+    printf("Tong tien hoa don mua hang: %.2f VND\n", bill);
+    printf("Giam gia: %.2f VND\n", discount);
+    printf("So tien can phai tra: %.2f VND\n", pay);
+    printf("------------------------------\n");
+
+    return 0;
+}
