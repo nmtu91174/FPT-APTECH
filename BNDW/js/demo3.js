7e5d6f8f769d1d1a77a34c25308224625c69380f
nmtu91174 <nmtu91174@gmail.com>
2025-09-19 19:46:48 +0700

    modif

--- BNDW/js/demo3.js
+++ BNDW/js/demo3.js
@@ -6,20 +6,30 @@ function demoTwo(){
     alert("demo to two");
 }
 
+var flag = false;
+var fanbox = null;
+
 function fanRotate() {
-    var fan = document.getElementById("fan");
-    var dg = 0;
+    if(flag == false){
+        var fan = document.getElementById("fan");
+        var dg = 0;
 
-    var fanbox = setInterval(function(){
-    fan.style.transform = `rotate(${dg}deg)`; //`` dấu này không phải nối chuỗi
-    dg+=180;
-    },1);
+        var fanbox = setInterval(function(){
+        fan.style.transform = `rotate(${dg}deg)`; //`` dấu này không phải nối chuỗi
+        dg+=180;
+         },1);
 
-    var btn = document.getElementById("btn-fan");
-    btn.innerText = "Stop";
-    
+        // var btn = document.getElementById("btn-fan");
+        btn.innerText = "Stop";
+        flag = true;
+    }else{
+        clearInterval(fanbox);
+        btn.innerText = "Start";
+        flag = false;
+        }
 }
-
+    
+    
 
 /* Cần có giao diện đặt các kích hoạt sự kiện
 - input bàn phím và chuột
